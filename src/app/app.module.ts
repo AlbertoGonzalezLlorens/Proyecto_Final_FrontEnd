505f41f2323ecbe9c853dea17df2de0d53fc8e7f
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-12-07 12:54:47 +0100

    Quitar micomponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MicomponenteComponent } from './micomponente/micomponente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MicomponenteComponent
   ],
   imports: [
     BrowserModule,
