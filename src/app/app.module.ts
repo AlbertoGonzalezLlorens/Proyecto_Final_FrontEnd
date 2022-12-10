96117288253c369b3545c77399a2be8dec2f2d4c
ivanhrt <ivanherreror@gmail.com>
2022-12-10 12:14:34 +0100

    re

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
 import { ElementsModule } from "./elements/elements.module";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -26,7 +27,8 @@ import { ElementsModule } from "./elements/elements.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ElementsModule
+        ElementsModule,
+        FormsModule
     ]
 })
 export class AppModule { }
