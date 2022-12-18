61f2e5669287997b610fb49e24a541f7786fa0ad
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-12-18 01:53:40 +0100

    coments

--- src/app/services/auth.interceptor.service.ts
+++ src/app/services/auth.interceptor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { TokenStorageService } from './token-storage.service';
@@ -22,4 +22,4 @@ export class AuthInterceptor implements HttpInterceptor {
 
 export const authInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
-];
+];*/
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,14 +24,14 @@ export class AuthService {
     }, httpOptions)
   }
 
-  register(username: string, nombre: string, apellidos: string, email:string, password: string,): Observable<any>{
+  /*register(username: string, nombre: string, apellidos: string, email:string, password: string,): Observable<any>{
     return this.http.post(AUTH_API+'api/hoteles', {
       username,
       nombre,
       apellidos,
       password
     }, httpOptions);
-  }
+  }*/
 
   isAuth():boolean{
     const token:any = this.tokenStorage.getToken();
