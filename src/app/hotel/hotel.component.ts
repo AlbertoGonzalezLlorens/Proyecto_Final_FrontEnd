e3ce13624de31a6f19cdd34377aa26a3b11cd047
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-12-19 13:31:00 +0100

    Cosiquis

--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ConsultasService } from '../services/consultas.service';
 import { ShareMapsDataService } from '../share-maps-data.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-hotel',
@@ -13,21 +14,31 @@ export class HotelComponent implements OnInit {
 
   habitaciones: any= null;
   contacto: any= null;
+  boton_reserva:number=0;
+  precio:number=0;
 
-
-  constructor(private shareDataService: ShareMapsDataService, private http: HttpClient,public consultaService: ConsultasService){}
+  constructor(private shareDataService: ShareMapsDataService, private http: HttpClient,public consultaService: ConsultasService,
+    private authservice:AuthService){}
 
   text: any = 0
   ngOnInit(): void {
-    this.http.get("https://proyectofinalapi-production-7f34.up.railway.app/api/habitaciones").subscribe(result=>{
-      this.habitaciones=result;
-    },
-    )
     this.http.get(`https://proyectofinalapi-production-7f34.up.railway.app/api/hoteles/contactos/${this.consultaService.hoteles.id_hotel}`).subscribe(result=>{
       this.contacto=result;
       //console.log("contacto",this.contacto)
     },
     )
+    this.http.get(`https://proyectofinalapi-production-7f34.up.railway.app/api/habitaciones/hotel/${this.consultaService.hoteles.id_hotel}`).subscribe(result=>{
+    this.habitaciones = result;
+    },
+    )
+
+    if(this.authservice.isAuth()){
+      this.boton_reserva=1;
+    } else {
+      this.boton_reserva=0;
+    }
+
+    console.log(this.consultaService.hoteles)
   }
 }
 
