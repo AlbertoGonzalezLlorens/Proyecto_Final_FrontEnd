7a5e534cd76a1041d96af37de1d3f8db4e30be3c
ivanhrt <ivanherreror@gmail.com>
2022-12-20 17:10:44 +0100

    holiwi

--- src/app/backoffice/usuarios/usuarios.component.ts
+++ src/app/backoffice/usuarios/usuarios.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent {
+
+  usuarios: any;
+
+  nombre: string = '';
+  username: number = 0;
+  precio_minimo: number = 0;
+  pais: string = 'España';
+  ciudad: string = 'Barcelona';
+  latitud: number = 41;
+  longitud: number = 3;
+  id_usuario: number=500;
+  hotelNuevo: any;
+
+
+
+  constructor( private http: HttpClient, private userService: UserService){}
+
+  ngOnInit(): void {
+    this.http.get("https://proyectofinalapi-production-7f34.up.railway.app/api/usuarios/").subscribe(result=>{
+      this.usuarios=result;
+      console.log(this.usuarios)
+
+    }
+    )
+
+  }
+
+
+  editHotel(usuario: any){
+    //Edita el hotel actual
+
+  }
+  deleteUsuario(id_usuario: any){
+    this.http.delete(`https://proyectofinalapi-production-7f34.up.railway.app/api/usuarios/${id_usuario}`).subscribe(result=>{
+
+
+    }
+    )
+
+  }
+
 
 }
